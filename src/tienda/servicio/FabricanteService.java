ec0dafe99ca771e10159ef67761c47f325f03c04
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-09-27 22:58:09 -0300

    Update FabricanteService

--- src/tienda/servicio/FabricanteService.java
+++ src/tienda/servicio/FabricanteService.java
@@ -1,5 +1,9 @@
 package tienda.servicio;
 
+import java.util.Collection;
+
+import com.sun.javadoc.ThrowsTag;
+
 import tienda.entidades.Fabricante;
 import tienda.persistencia.FabricanteDAO;
 
@@ -21,7 +25,8 @@ public class FabricanteService {
 	
 	/**
 	 * Valida los datos pasados por parametro, y si todo esta Ok, crea un nuevo
-	 * fabricante y lo guarda en la BD
+	 * fabricante, llama al metodo guardarFabricante de la clase fabricanteDAO, 
+	 * y este hace el registro en la Base De datos.
 	 * 
 	 * @param codigo
 	 * @param nombre
@@ -61,6 +66,7 @@ public class FabricanteService {
 	
 	/**
 	 * Recibe un nombre por parametro y valida si existe o no el resgistro en la BD.
+	 * Llamando al metodo buscarfabricantePorNombre de la clase FabricanteDAO
 	 * 
 	 * @param nombre
 	 * @return Obj Fabricante si existe en la BD
@@ -79,6 +85,102 @@ public class FabricanteService {
 			Fabricante fabricante = dao.buscarFabricantePorNombre(nombre);
 			
 			return fabricante;
+			
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	/**
+	 * Recibe un nombre por parametro y valida si existe o no el resgistro en la BD.
+	 * Llamando al metodo buscarfabricantePorNombre de la clase FabricanteDAO
+	 * 
+	 * @param nombre
+	 * @return Obj Fabricante si existe en la BD
+	 * @throws Exception
+	 */
+	public Fabricante buscarFabricantePorCodigo(Integer codigo) throws Exception {
+		
+		try {
+			
+			// Validamos
+			if (codigo == null) {
+				throw new Exception("Debe indicar el codigo del Fabricante.");
+			}
+			
+			// Usamos el dao para llamar al metodo y traer el resultado.
+			Fabricante fabricante = dao.buscarFabricantePorCodigo(codigo);
+			
+			return fabricante;
+			
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	/**
+	 * Recibe el codigo por parameto y llama al metodo eliminarFabricante
+	 * de la clase FabricanteDAO
+	 * 
+	 * @param codigo
+	 * @throws Exception
+	 */
+	public void eliminarFabricante(Integer codigo) throws Exception {
+		
+		try {
+			
+			// Validamos
+			if (codigo == null) {
+				throw new Exception("Debe indicar el codigo del Fabricante a eliminar.");
+			}
+			
+			// Usamos el dao para llamar al metodo y eliminar el registro
+			dao.eliminarFabricante(codigo);
+			
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	/**
+	 * llama al metodo listarFabricantes del DAO y crea la lista con los registros de la tabla.
+	 * Dicha lista servira para imprimir o mostrar los registros por pantalla.
+	 * 
+	 * @return fabricantes equivale a una lista con todos los registros de la tabla
+	 * @throws Exception
+	 */
+	public Collection<Fabricante> listarFabricantes() throws Exception {
+		
+		try {
+			
+			Collection<Fabricante> fabricantes = dao.listarFabricantes();
+			
+			return fabricantes;
+			
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	/**
+	 * Llama al metodo listarFabricantes del service, para mostrar los registros de la tabla Fabricante
+	 */
+	public void imprimirFabricantes() throws Exception {
+		
+		try {
+			
+			// Listamos los fabricantes
+			Collection<Fabricante> fabricantes = listarFabricantes();
+			
+			// Imprimimos o mostramos los fabricantes
+			if (fabricantes.isEmpty()) {
+				throw new Exception("No existen fabricantes para imprimir.");
+			} else {
+				// iteramos en un for para mostrar todos los registros
+				for (Fabricante f : fabricantes) {
+					System.out.println(f);
+				}
+			}
 			
 		} catch (Exception e) {
 			throw e;
