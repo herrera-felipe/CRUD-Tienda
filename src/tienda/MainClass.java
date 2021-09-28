5541cc602819a3874ac02ade36e64f9b16d0b0b8
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-09-27 22:58:46 -0300

    Update MainClass

--- src/tienda/MainClass.java
+++ src/tienda/MainClass.java
@@ -1,5 +1,6 @@
 package tienda;
 
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,10 +18,25 @@ public class MainClass {
 
 		FabricanteService fabricanteService = new FabricanteService(); // Instanciamos el Service
 		
+		Scanner sc = new Scanner(System.in).useDelimiter("\n");
+		
+		int codigo;
+		String nombre;
+		
 		try {
 			// Creamos un nuevo Fabricante
-			fabricanteService.crearFabricante(10, "Nuevo Fabricante");
+//			System.out.println("Ingrese el codigo del Fabricante:");
+//			codigo = Integer.parseInt(sc.nextLine());
+//			
+//			System.out.println("Ingrese el nombre del Fabricante:");
+//			nombre = sc.next();
+//			
+//			// Prueba de Metodos
+//			fabricanteService.crearFabricante(codigo, nombre); // Llamada del metodo mediante el service
 			
+//			fabricanteService.eliminarFabricante(codigo);
+			
+			fabricanteService.imprimirFabricantes();
 		} catch (Exception e) {
 			Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, e);
 		}
