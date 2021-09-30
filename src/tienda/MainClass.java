7f54f52faee4c10004d78c5ad2953d83fe3d677f
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-09-30 11:02:58 -0300

    Update MainClass

--- src/tienda/MainClass.java
+++ src/tienda/MainClass.java
@@ -22,39 +22,42 @@ public class MainClass {
 		String nombre;
 		double precio;
 
-		// A) Listamos los nombres de los productos
-//		try {
-//			System.out.println("[ NOMBRES PRODUCTOS ]");
-//			productoService.imprimirNombreProductos();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			System.out.println("Error del sistema por \n" + e.getMessage());
-//		}
-//		
-//		
-//		// B) Lista los nombres y los precios de los productos
-//		try {
-//			System.out.println("\n[ PRODUCTOS CON PRECIO ]");
-//			productoService.imprimirNombrePrecioProductos();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			System.out.println("Error del sistema por \n" + e.getMessage());
-//		}
+		// Listamos TODOS los nombres de los productos
+		try {
+			System.out.println("[ NOMBRES PRODUCTOS ]");
+			productoService.imprimirNombreProductos();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error del sistema por \n" + e.getMessage());
+		}
 		
-//		// C) Listar por rango de precio
-//		try {
-//			System.out.println("\n[ PRODUCTOS POR RANGO DE PRECIO ]");
-//			System.out.println("Ingrese los valores de rango de precio entre los que quiere filtrar los productos:");
-//			num1 = Integer.parseInt(sc.nextLine());
-//			num2 = Integer.parseInt(sc.nextLine());
-//			
-//			productoService.imprimirPorRangoPrecio(num1, num2);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			System.out.println("Error del sistema por \n" + e.getMessage());
-//		}
 		
-		// D) Buscar producto por nombre
+		// Lista los nombres y los precios de los productos
+		try {
+			System.out.println("\n[ PRODUCTOS CON PRECIO ]");
+			productoService.imprimirNombrePrecioProductos();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error del sistema por \n" + e.getMessage());
+		}
+		
+		
+		// Listar Producto por rango de precio
+		try {
+			System.out.println("\n[ PRODUCTOS POR RANGO DE PRECIO ]");
+			System.out.println("Ingrese los valores de rango de precio entre los que quiere filtrar los productos:");
+			num1 = Integer.parseInt(sc.nextLine());
+			num2 = Integer.parseInt(sc.nextLine());
+			
+			productoService.imprimirPorRangoPrecio(num1, num2);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error del sistema por \n" + e.getMessage());
+		}
+		
+		
+		
+		// Buscar producto por nombre
 		try {
 			System.out.println("\n[ BUSCAR PRODUCTO POR NOMBRE ]");
 			System.out.println("Ingrese el nombre del producto:");
@@ -64,48 +67,51 @@ public class MainClass {
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		// H) Modificar producto a medias, el usuario debe seleccionar que valor modificar
-//		try {
-//			System.out.println("\n[ MODIFICACION DE PRODUCTO ]");
-//			System.out.println("Ingrese el codigo del producto que desea modificar:");
-//			codigo = Integer.parseInt(sc.nextLine());
-//			
-//			System.out.println("[ DATOS DEL PRODUCTO A MODIFICAR ]");
-//			System.out.println(productoService.buscarProductoPorCodigo(codigo));
-//			
-//			System.out.println("\nIngrese el nuevo nombre del producto:");
-//			nombre = sc.nextLine();
-//			
-//			System.out.println("Ingrese el precio del producto:");
-//			precio = Double.parseDouble(sc.nextLine());
-//			
-//			System.out.println("Ingrese el codigo del fabricante del producto:");
-//			codigo_fabricante = Integer.parseInt(sc.nextLine());
-//			
-//			productoService.modificarProducto(codigo, nombre, precio, codigo_fabricante);
-//			productoService.imprimirNombreProductos();
-//			
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			System.out.println("Error del sistema por \n" + e.getMessage());
-//		}
-//
-//		// G) Creamos un nuevo Fabricante
-//		try {
-//
-//			System.out.println("Ingrese el codigo del Fabricante:");
-//			codigo_fabricante = Integer.parseInt(sc.nextLine());
-//
-//			System.out.println("Ingrese el nombre del Fabricante:");
-//			nombre = sc.next();
-//
-//			fabricanteService.crearFabricante(codigo_fabricante, nombre); // Llamada del metodo mediante el service
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			System.out.println("Error del sistema por \n" + e.getMessage());
-//		}
+		
+		
+		// Modificar producto
+		try {
+			System.out.println("\n[ MODIFICACION DE PRODUCTO ]");
+			System.out.println("Ingrese el codigo del producto que desea modificar:");
+			codigo = Integer.parseInt(sc.nextLine());
+			
+			System.out.println("[ DATOS DEL PRODUCTO A MODIFICAR ]");
+			System.out.println(productoService.buscarProductoPorCodigo(codigo));
+			
+			System.out.println("\nIngrese el nuevo nombre del producto:");
+			nombre = sc.nextLine();
+			
+			System.out.println("Ingrese el precio del producto:");
+			precio = Double.parseDouble(sc.nextLine());
+			
+			System.out.println("Ingrese el codigo del fabricante del producto:");
+			codigo_fabricante = Integer.parseInt(sc.nextLine());
+			
+			productoService.modificarProducto(codigo, nombre, precio, codigo_fabricante);
+			productoService.imprimirNombreProductos();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error del sistema por \n" + e.getMessage());
+		}
 
-	} // fin main
+		
+		// Creamos un nuevo Fabricante
+		try {
 
-} // fin class
+			System.out.println("Ingrese el codigo del Fabricante:");
+			codigo_fabricante = Integer.parseInt(sc.nextLine());
+
+			System.out.println("Ingrese el nombre del Fabricante:");
+			nombre = sc.next();
+
+			fabricanteService.crearFabricante(codigo_fabricante, nombre); // Llamada del metodo mediante el service
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error del sistema por \n" + e.getMessage());
+		}
+
+	}
+
+} 
